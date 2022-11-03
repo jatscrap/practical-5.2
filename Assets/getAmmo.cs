e95499cd681d31be21e90dd11b97a4781598cd11
jatscrap <jt1755@york.ac.uk>
2022-11-03 11:26:21 +0000

    practical not done

---
+++ Assets/getAmmo.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getAmmo : MonoBehaviour
+{
+    public GameObject AmmoBox; 
+    int ammoCount = 0; //ammo count variable
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      RaycastHit hitInfo;
+    Ray ray =  Camera.main.ViewportPointToRay(new Vector3(0.5f ,0.5f ,0.0f));
+    bool hit = Physics.Raycast(ray, out hitInfo);
+    if (Input.GetButtonDown("Fire1")) {
+        ammoCount--;
+    }
+
+    if (hitInfo.collider.gameObject.name == "Target") {
+        print (hitInfo.collider.gameObject.parent.name);
+
+    } 
+    
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "AmmoBox") { //if the object being collided with is "AmmoBox"
+            other.gameObject.SetActive(false); //hide AmmoBox
+            ammoCount = 20; //set ammoCount to 20 when the box is picked up
+            Debug.Log("hello"); //make sure the collider is bigger than the table so a collision is detected
+        }
+        //AmmoBox = GameObject.Find("AmmoBox"); 
+        //AmmoBox.SetActive(false);
+    }
+
+   
+
+}
---
+++ Assets/openSesame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openSesame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+
+    }
+    //this function needs to be outside update because it is a separate event handler
+    //this function takes a Collider object as a parameter rather than a Collision, because no physical collision takes place
+    void OnTriggerEnter(Collider other) { //allows us to detect when 2 colliders intersect
+       GameObject parent = transform.parent.gameObject;       
+       Animation animation = parent.GetComponent<Animation>();       
+       animation.Play("OpenDoor");      
+     } 
+}
